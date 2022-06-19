1cd27d6fbd5c5091a35b90860792511a7ee83eaa
Руслан Прусаков <ruslan.prusakov@gmail.com>
2022-06-19 23:25:37 +0300

    new dz

--- date.py
+++ date.py
@@ -65,7 +65,125 @@
 # name = 'abrakadabra'
 # print(name.replace("ab", ""))
 
-places = [20, 40, 100, 32, 54, 78, 12, 4, 99]
-car = 350
-weight = []
+# web_dict = {"title":"Web разраб",
+# "duration":19,
+# "mentors":["nicola", "alena", "alex", "aleksandr", ]}
+# frontend_dict = {"title":"front", 
+# "duration":13, "mentors":["coach_1", "coach_2", "coach_3"]}
+# java_dict = {"title":"Java",
+# "duration":11, "mentors":["coach_4","coach_5","coach_6"]}
+
+# courses_list = [web_dict, frontend_dict, java_dict]
+# max_count = 0
+# course_id = None
+
+# for id, course in enumerate(courses_list):
+#     mentors_count = len(course["mentors"])
+#     # print(f'На курсе {course["title"]} {mentors_count} преподавателей')
+#     if mentors_count > max_count:
+#         max_count = mentors_count
+#         course_id = id
+# max_course = courses_list[course_id]
+# print(f'Самый крутой курс {max_course["title"]} потому что на нем {max_count} преподавателей')
+
+# students_list = [
+#     {"name": "Василий", "surname": "Теркин", "gender": "м", "program_exp": True, "grade": [8, 8, 9, 10, 9], "exam": 8},
+#     {"name": "Мария", "surname": "Павлова", "gender": "ж", "program_exp": True, "grade": [7, 8, 9, 9], "exam": 9},
+#     {"name": "Ирина", "surname": "Андреева", "gender": "ж", "program_exp": False, "grade": [10, 9, 8, 10, 10], "exam": 7},
+#     {"name": "Татьяна", "surname": "Сидорова", "gender": "ж", "program_exp": False, "grade": [7, 9, 8],"exam": 10},
+#     {"name": "Иван", "surname": "Васильев", "gender": "м", "program_exp": True, "grade": [9, 8, 9, 6, 9], "exam": 5},
+#     {"name": "Роман", "surname": "Золотарев", "gender": "м", "program_exp": False, "grade": [8, 9, 9, 6, 9], "exam": 6}
+# ]
+# def get_avg_ex_grade(students):
+#   sum_ex = 0
+#   for student in students:
+#     sum_ex += student['exam']
+#   return round(sum_ex / len(students), 2)
+
+
+# print(get_avg_ex_grade(students_list))
+
+# def avd_hw_grade(students, exp):
+#   sum_hw = 0
+#   counter = 0
+#   for student in students:
+#     if student ['program_exp'] == exp:
+#       sum_hw += sum(student['grade']) / len(student['grade'])
+#       counter +=1
+#   return round(sum_hw / counter, 2)
+
+
+# print(avd_hw_grade(students_list, True))
+# print(avd_hw_grade(students_list, False))
+
+# def fire_student(students):
+#   students_copy = students.copy()
+#   for student in students_copy:
+#     if sum(student['grade']) / len(student['grade']) < 8.5:
+#       students.remove(student)
+
+
+# fire_student(students_list)
+
+# print(students_list)
+
+
+
+documents = [
+        {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+        {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+        {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
+        {"type": "passport", "number": "5455 028765", "name": "Иван Иванов"},
+        {"type": "passport", "number": "2212 645321", "name": "Петр Петров"},
+        {"type": "passport", "number": "9215 900076", "name": "Василий Васильев"},
+        {"type": "passport", "number": "4532 765987", "name": "Антон Куков"},
+        {"type": "passport", "number": "987 654321", "name": "Михаил Фридман"},
+        {"type": "invoice", "number": "13-7", "name": "Константин Пупкин"},
+        {"type": "invoice", "number": "14-5", "name": "Олег Костин"}
+]
+directories = {
+        '1': ['2207 876234', '11-2', '5455 028765'],
+        '2': ['10006', '4532 765987', '13-7'],
+        '3': ['2212 645321', '9215 900076', '14-5'],
+        '4': ['987 654321'],
+        '5': []
+}
+
+def search_people(documents):
+  doc_numb =input("doc number?: ")
+  for doc in documents:
+    if doc_numb == doc['number']:
+      return f' {doc["name"]} {doc["number"]}'
+  else:
+    print("no this document")
+
+def shelf(directories):
+  doc_numb = input("doc number?: ")
+  for doc_place in directories:
+    if doc_numb in directories[doc_place]:
+      return f'{doc_numb} находится на полке {doc_place}'
+  else:
+    return "NO DOCUMENT"
+
+def doc_list(documents):
+  for doc in documents:
+    print(doc["type"], doc["number"], doc["name"])
+
+
+def main():
+  while True:
+    comand = input("comand?: ")
+    if comand == "p":
+      print(search_people(documents))
+
+
+  
+
+
+
+
+
+
+
+
 
